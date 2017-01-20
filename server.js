3ac76a2c9dad89f1faf58737482bac267c837990
Antoine BERNIER <antoine.bernier@gmail.com>
2017-01-20 16:40:18 +0100

    env port

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port '+port+'!')
 })
